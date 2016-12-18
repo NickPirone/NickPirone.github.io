d11cc53d88545f4cb4e45aefe1dc3967896ef79a
Nicoroney <none>
2016-12-18 12:33:18 -0600

    got all guitar frets built.  working on assigning notes

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,12 +1,16 @@
+var notes = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B"];
+
 var main = function(){
 	buildGuitarLayout();
+	console.log(notes.length);
 }
 
 var buildGuitarLayout = function(){
 	var fretLength = 100;
-	for(i = 0; i < 1; i++)
+	for(i = 0; i < 24; i++)
 	{
 		var rowOfFrets = $('<div class="fret">');
+		rowOfFrets.height(fretLength);
 		rowOfFrets.appendTo($('.guitar-display'));
 		for(j = 0; j < 6; j++)
 		{
@@ -15,7 +19,7 @@ var buildGuitarLayout = function(){
 			note.height(fretLength);
 			note.appendTo(rowOfFrets);
 		}
-		fretLength = fretLength * .9;
+		fretLength = fretLength * .95;
 	}
 };
 
