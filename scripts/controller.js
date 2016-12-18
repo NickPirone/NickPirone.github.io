ce1dd14e142380475c797b4207bd678918f813eb
Nicoroney <none>
2016-12-18 07:46:29 -0600

    first work on building guitar layout.  Have basic jquery loop set up

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-        $("button").click(function(){
-                $("p").hide();
-        });
-});
+var main = function(){
+	buildGuitarLayout();
+}
+
+var buildGuitarLayout = function(){
+	for(i = 0; i < 1; i++)
+	{
+		var fretLength = 20;
+		for(j = 0; j < 6; j++)
+		{
+			var fret = $('<div class="fret">');
+			fret.appendTo($('.guitar-display'));
+		}
+	}
+};
+
+
+
+
+
+$(document).ready(main);
